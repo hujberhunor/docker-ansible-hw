bd7803cb2f4a5434cb47a0c4210cb062e89e72ea
Hujber Hunor <hujber.hunor@gmail.com>
2025-11-20 10:02:49 +0100

    ssh script copies pub key to the curr users .ssh dir

--- gen-ssh.sh
+++ gen-ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -f "docker/id_ed.pub" ]; then 
-	    ssh-keygen -t ed25519 -f "docker/id_ed" -N "" 
+if [ ! -f ~/.ssh/id_ed.pub ]; then 
+	    ssh-keygen -t ed25519 -f ~/.ssh/id_ed -N "" 
     else
 	        echo "SSH key is already there"
 fi 
